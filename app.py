86d97cb12f7a9ac76dca00895723f3c763aecb05
KelvynmPaiva <kelvynmpaiva@gmail.com>
2025-05-21 12:39:07 -0300

    Mudando estado do restaurante

--- app.py
+++ app.py
@@ -61,6 +61,24 @@ def voltar_ao_menu():
 def limpar_tela():
     os.system("cls")
 
+def alternar_estado_restaurante():
+    limpar_tela()
+    print("Alterando estado do restaurante \n")
+
+    nome_restaurante = input("Digite o nome do restaurante que deseja aterar o estado:")
+    restaurante_encontrado = False
+
+    for restaurante in restaurantes:
+        if nome_restaurante == restaurante['nome']:
+            restaurante_encontrado = True
+            restaurante['ativo'] = not restaurante['ativo']
+            mensagem = f"O restaurante {nome_restaurante} foi ativado com sucesso!" if restaurante['ativo'] else f"O restaurante {nome_restaurante} foi desativado com sucesso!"
+            print(mensagem)
+    if not restaurante_encontrado:
+        print("O restaurante não foi encontrado!")
+
+    voltar_ao_menu()
+
 def escolher_opcao():
     try:
         opcao_escolhida = int(input('Escolha uma opção: '))
@@ -70,7 +88,7 @@ def escolher_opcao():
         elif opcao_escolhida == 2: 
             listar_restaurantes()
         elif opcao_escolhida == 3: 
-            print('Ativar restaurante')
+            alternar_estado_restaurante()
         elif opcao_escolhida == 4: 
             finalizar_app()
         else: 
