c458dfb09885e9ab48082e3f51d62b01b38935fa
Kelvyn Paiva <kelvynmpaiva@gmail.com>
2025-05-21 23:28:13 -0300

    Atributos de instância

---
+++ modelos/restaurante.py
@@ -0,0 +1,10 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = Restaurante()
+restaurante_praca.nome = 'Praça'
+restaurante_praca.categoria = "Gourmet"
+
+print(vars(restaurante_praca))
